d2019e2d7d026d2d4c535aa78b5546a5cbf6b6e2
Victorine Penot <vpenot001@bordeauc-inp.fr>
2022-11-14 16:23:51 +0100

    Courbe d'autocorr

--- BruitBlancGaussien.m
+++ BruitBlancGaussien.m
@@ -6,4 +6,23 @@ SigmaSym=1;
 Puimoy=SigmaSym*PuiFil./Ts;
 sigma2=SigmaSym*PuiFil./(2*10.^(dB/10));
 
-bbg=(sqrt(sigma2)*randn(n,1)+1j*randn(n,1));
+bbg=sqrt(sigma2)*randn(n,1);
+l=meshgrid(-n+1:1:n-1,1);
+AutoCorr=zeros(2*n-1,1);
+AutoCorr(n)=sigma2;
+unbia=xcorr(bbg,'unbiased');
+bia=xcorr(bbg,'biased');
+%% Figure
+
+%Representation autocorrélation théorique:
+
+figure,
+subplot(3,1,1);
+plot(l,AutoCorr);
+title("Autocorrélation théorique");
+subplot(3,1,2);
+plot(l,unbia);
+title("Corrélatioon non-biaisé");
+subplot(3,1,3);
+plot(l,bia);
+title("Corrélatioon biaisé");
--- main.m
+++ main.m
@@ -4,10 +4,12 @@ clc,
 
 sig=load("fcno03fz.mat");
 s1 = -sig.fcno03fz;
-
+n=length(s1);
+[bbg]=BruitBlancGaussien(length(s1),1,5,1e-6);
 
 %% Figure
 
 figure,
 plot(s1);
 title("Signal original");
+
