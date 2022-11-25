586e88d05242b745ea2dafa138eebaa969548d0d
Julie Messner <julie.messner@enseirb-matmeca.fr>
2022-11-25 12:04:59 +0100

    Parole bruitée complète

--- main.m
+++ main.m
@@ -7,7 +7,23 @@ s1 = -sig.fcno03fz;
 n=length(s1);
 [bbg]=BruitBlancGaussien(length(s1),1,5,1e-6);
 
-sigbruite = parolebruitee(s1, bbg);
+%% Ajout du bruit en fonction du RSB final 
+
+RSB = 5;
+
+sigbruite = parolebruitee(s1, RSB);
+
+% RSB = 10;
+% 
+% sigbruite2 = parolebruitee(s1, RSB);
+% RSB = 15;
+% 
+% sigbruite3 = parolebruitee(s1, RSB);
+
+
+%% Traitement de trame
+
+res = traitement_trame(sigbruite);
 
 %% Figure
 
---
+++ traitement_trame.m
@@ -0,0 +1,14 @@
+function Sigrehausse = traitement_trame(SigBruitFT)
+
+dspestim = Mon_Welch(SigBruitFT,512);
+
+Sigrehausse = SigBruitFT - dspestim;
+
+n = length(Sigrehausse);
+
+for i=1:n
+    if(Sigrehausse(i)<0)
+        Sigrehausse(i) = 0;
+    end
+end
+
