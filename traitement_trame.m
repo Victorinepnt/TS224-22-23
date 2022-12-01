9ccbbf6a8504db58f3786e4047db127e3dc4e8cd
Julie Messner <julie.messner@enseirb-matmeca.fr>
2022-12-01 12:19:51 +0100

    Mariah season

--- traitement_trame.m
+++ traitement_trame.m
@@ -7,11 +7,9 @@ sigma2 = var(extrait);
 SigBruitFT = fft(SigBruit);
 n = length(SigBruitFT);
 
-bruit=sqrt(sigma2)*randn(n,1);
+dspestim = ones(n,1)*sigma2;  %problème de taille pour la soustraction !!
 
-dspestim = Mon_Welch(bruit',n);  %problème de taille pour la soustraction !!
-
-Sigrehausse = abs(SigBruitFT) - abs(dspestim');
+Sigrehausse = abs(SigBruitFT) - abs(dspestim);
 
 for i=1:n
     if(Sigrehausse(i)<0)
@@ -19,9 +17,17 @@ for i=1:n
     end
 end
 
-SigdebruitFT = Sigrehausse + angle(SigBruitFT);
+figure,
+plot(Sigrehausse);
+title('Sigrehausse');
 
-Sigdebruit = fftshift(ifft(SigdebruitFT));
+SigdebruitFT = Sigrehausse; %+ angle(SigBruitFT);
+
+figure,
+plot(SigdebruitFT);
+title('SigdebruitFT');
+
+Sigdebruit = ifft(SigdebruitFT); 
 
 figure,
 plot(Sigdebruit);
